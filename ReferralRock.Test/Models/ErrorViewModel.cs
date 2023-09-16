dc33a34773a1a19b77b53c3922fd4ba8f249a1db
AlbatrosWork <albatros.sherifi.work@gmail.com>
2023-09-16 02:54:45 +0200

    initial commit

---
+++ ReferralRock.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReferralRock.Test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
